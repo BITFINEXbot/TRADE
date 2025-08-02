05d19934704012711872792b5002905978eeb1ec
BITFINEXbot <domtoretto5566gt@gmail.com>
2025-08-02 12:52:32 +0300

    Update sentiment_analyzer.py

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,5 +1,5 @@
-
 def update_sentiment(symbol):
-    # Симулиран sentiment score между -1 и 1
     import random
-    return random.uniform(-0.5, 0.5)
+    # Връща стойности между -1.0 и 1.0
+    return random.uniform(-1.0, 1.0)
+
